8b3e97f16ebf28ec1c920b30394c1cfa23897b5a
hshiue <hilaryszuyinshiue@nypl.org>
2023-07-14 12:23:07 -0400

    add 'add_dest_to_dict function'

--- mv_towaitingforprsv.py
+++ mv_towaitingforprsv.py
@@ -9,15 +9,27 @@ def _make_parser():
     parser.add_argument('-o', '--origin',
                         help = '''required. The original directory that contains the AMI bag''',
                         required=True)
+    parser.add_argument('-d', '--destination',
+                        help = '''required. The direct path to 0_waiting_for_preservica''',
+                        required=True)
     return parser
 
-def find_bags(dir_path: str) -> dict:
+def find_bags(ori_path: str) -> dict:
     bag_dict = dict()
-    basepath = Path(dir_path) # /Volumes/parking/lpa/1_prestaging/2023-06-07
+    basepath = Path(ori_path) # /Volumes/parking/lpa/1_prestaging/2023-06-07
     for pjpath in basepath.iterdir():
         for ami_bag in pjpath.iterdir():
             bag_dict[ami_bag.name] = []
             bag_dict[ami_bag.name].append(ami_bag)
+
+    return bag_dict
+
+def add_dest_to_dict(bag_dict, dest_dir) -> str:
+    basetarget = Path(dest_dir) # /Volumes/parking/lpa/0_waiting_for_preservica
+    for id in bag_dict:
+        first_three = id[0:3]
+        dest_path = basetarget / first_three / id
+        bag_dict[id].append(dest_path)
 
     return bag_dict
 
@@ -33,6 +45,7 @@ def main():
     args = parser.parse_args()
 
     bag_dict = find_bags(args.origin)
+    updated_bag_dict = add_dest_to_dict(bag_dict, args.destination)
 
 
 
