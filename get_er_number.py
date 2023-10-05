ac6d081c5e56c6f650c87c6c46416efcd00c2f24
hshiue <hilaryszuyinshiue@nypl.org>
2023-10-05 13:56:31 -0400

    update code. add in using xml method. update url so it does not care about division code

--- get_er_number.py
+++ get_er_number.py
@@ -3,22 +3,40 @@ import logging
 import re
 import requests
 import xml.etree.ElementTree as ET
+from pathlib import Path
+import sys
 
 def _make_parser():
     parser = argparse.ArgumentParser(description='get all ER number from NYPL Finding Aid')
     parser.add_argument('--collectionID', '-c',
                         help = '''required. number only, e.g. 18400 for M18400''',
-                        required=True)
+                        required=False)
 
     parser.add_argument('--find_dups', '-fd',
                         help="finding duplicates in the er, di and em list",
                         required=False,
                         action='store_true')
 
+    parser.add_argument('--folder_path', '-fp',
+                       help="optional. Direct path to the folder with FA XMLs",
+                       required=False)
+
+
     return parser
 
-def parse_xml_response(response):
+def prepare_api_response(response):
     root = ET.fromstring(response.text)
+
+    return root
+
+def prepare_xml_file(xml_file):
+    tree = ET.parse(xml_file)
+    root = tree.getroot()
+    print(root)
+
+    return root
+
+def parse_xml_text(root):
     ns = f"{{urn:isbn:1-931666-22-9}}"
 
     er_no_ls = list()
@@ -52,9 +70,6 @@ def find_dups(ls) -> set:
     return dups
 
 
-
-
-
 def main():
     """
     Insert a collection ID number (only) to get all ER, coded in
@@ -70,40 +85,56 @@ def main():
     parser = _make_parser()
     args = parser.parse_args()
 
-    url = f"https://nyplorg-data-archives.s3.amazonaws.com/uploads/collection/generated_xml/mss{args.collectionID}.xml"
-    response = requests.request("Get", url)
-    er_no, di_no, em_no = parse_xml_response(response)
+    url = f"https://archives.nypl.org/the/{args.collectionID}.xml"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        logging.info(f"response code 200")
+        root = prepare_api_response(response)
+        er_no, di_no, em_no = parse_xml_text(root)
+    elif args.folder_path:
+        logging.info(f"response code NOT 200. using folder path")
+        folder = Path(args.folder_path)
+        for i in folder.iterdir():
+            if args.collectionID in i.name:
+                root = prepare_xml_file(i)
+                er_no, di_no, em_no = parse_xml_text(root)
+    else:
+        logging.error(f"response status code not 200, {response.status_code}")
+        sys.exit()
+
+
     if er_no and check_consecutive(er_no):
         print(f"""Total er: {len(er_no)}
-                  numbers are consecutive""")
+                numbers are consecutive""")
     elif not er_no:
         print(f"""no ERs in M{args.collectionID}""")
     else:
         print(f"""ER are not consecutive. Review list manually
-              Total er: {len(er_no)}; largest er number: {er_no[-1]}""")
+            Total er: {len(er_no)}; largest er number: {er_no[-1]}""")
 
     if di_no and check_consecutive(di_no):
         print(f"""Total er: {len(di_no)}
-                  numbers are consecutive""")
+                numbers are consecutive""")
     elif not di_no:
         print(f"""no DIs in M{args.collectionID}""")
     else:
         print(f"""DI are not consecutive. Review list manually
-              Total er: {len(di_no)}""")
+            Total di: {len(di_no)}""")
 
     if em_no and check_consecutive(em_no):
         print(f"""Total er: {len(em_no)}
-                  numbers are consecutive""")
+                numbers are consecutive""")
     elif not em_no:
         print(f"""no EMs in M{args.collectionID}""")
     else:
         print(f"""EM are not consecutive. Review list manually
-              Total er: {len(em_no)}""")
+            Total em: {len(em_no)}""")
 
     print(f"""
-            er_no: {er_no},
-            di_no: {di_no},
-            em_no: {em_no}
+            er_no: {sorted(er_no)},
+            di_no: {sorted(di_no)},
+            em_no: {sorted(em_no)}
             """)
     if args.find_dups:
         print(f"""ER dups: {find_dups(er_no)}
